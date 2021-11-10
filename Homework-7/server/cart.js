169141cb91c0d8ac652e368ffb657a7342082647
Landesadel <iliillimusicforever@gmail.com>
2021-11-10 20:47:17 +0300

    дз8
    Небольшие изменения в верстке, доделано удаление из корзины

--- Homework-7/server/cart.js
+++ Homework-7/server/cart.js
@@ -8,11 +8,14 @@ let change = (cart, req) => {
     return JSON.stringify(cart, null, 4);
 };
 let remove = (cart, req) => {
-    let find = cart.contents.find(el => +el.id_product === req.body.id_product);
-    let cartContent = cart.contents;
-    cartContent.splice(cartContent.indexOf(find), 1);
+    let find = cart.contents.find(el => el.id_product === +req.params.id);
+    if (find.quantity > 1) {
+        find.quantity -= 1;
+    } else {
+        cart.contents.splice(cart.contents.indexOf(find), 1);
+    }
     return JSON.stringify(cart, null, 4);
-}
+};
 
 module.exports = {
     add,
